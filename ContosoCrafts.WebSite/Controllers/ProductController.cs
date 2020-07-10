b5050b7a6aa9e95b41d6a2485868766474c5a94e
thuydotp <phuonthuydt0311@gmail.com>
2020-07-10 10:53:21 +0700

    First commit

---
+++ ContosoCrafts.WebSite/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        private JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return this.ProductService.GetProducts();
+        }
+        
+        //[HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery]string productId, [FromQuery] int rating)
+        {
+            ProductService.AddRating(productId, rating);
+            return Ok();
+        }
+
+    }
+}
---
+++ ContosoCrafts.WebSite/Services/JsonFileProductService.cs
@@ -0,0 +1,63 @@
+﻿using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace ContosoCrafts.WebSite.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; set; }
+
+        private string JsonsFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using(StreamReader jsonFileReader = File.OpenText(JsonsFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions{ PropertyNameCaseInsensitive = true});
+            }
+        }
+
+        public void AddRating(string productId, int rating)
+        {
+            IEnumerable<Product> products = GetProducts();
+
+            //LINQ
+            var query = products.FirstOrDefault(x => x.Id == productId);
+
+            if(query == null) { return; }
+
+            if(query.Ratings == null)
+            {
+                query.Ratings = new int[] { rating };
+            }
+            else
+            {
+                List<int> ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
+
+            using(var outputStream = File.OpenWrite(JsonsFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions { 
+                        SkipValidation = true, Indented = true
+                    }),
+                    products);
+            }
+        }
+    }
+}
